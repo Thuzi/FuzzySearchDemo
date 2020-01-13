50debeb4625a711f3b69aacdae37af7b9a4f0138
Michael Isasi <Misasi@thuzi.com>
2020-01-13 10:03:53 -0500

    Initial Commit

---
+++ FuzzyTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FuzzyTest2
+//
+//  Created by Michael Isasi on 1/13/20.
+//  Copyright © 2020 Michael Isasi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
